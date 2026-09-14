b0b0aa49d9853c62c56414c2cfaf87c4cd2a928f
Semba Mbasela <semba@Sembas-Air.gateway.sasktel.net>
2026-09-13 20:31:02 -0600

    Practice arrays, objects, and map with React

---
+++ react/arrays-and-map/README.md
@@ -0,0 +1,27 @@
+# Arrays and map() in React 
+
+## What I Learned 
+
+### Arrays
+[Explain what an array is in your own words.]
+Arrays are structures that store objects. This will help with the storage of tickets in my case.  
+
+### Objects 
+[Explain what a JavaScript object is.]
+A Javascript object is variable that can be passed and evaluated by java functions. 
+
+## map()
+[Explain what map() does.]
+Map takes elements of an array, transforms them, and returns a new array back. 
+
+## React Keys 
+[Explain why key = {ticket.id} was necessary]
+
+React wants this when we are generating multiple tickets so there is a way to keep track of them when modifications are made (adding removing,reordering, and updating tickets) 
+
+## How I'll Use This 
+
+[Explain how this could eventually be useful in the Help Desk project.] 
+
+This can be useful when working with multiple tickets as it can be a way to process multiple tickets in a systematic way. 
+
--- react/components-and-props/App.jsx
+++ react/components-and-props/App.jsx
@@ -1,14 +1,45 @@
 import TicketCard from "./TicketCard";
 
+const tickets = [
+    {
+        id: 1,
+        title: "Cannot connect to Wi-Fi",
+        description: "User cannot connect to office Wi-Fi",
+        priority: "High",
+        status: "Open"
+    },
+    {
+        id: 2,
+        title: "Printer not working",
+        description: "User cannot connect to office printer",
+        priority: "Low",
+        status: "Open"
+    },
+    {
+        id: 3,
+        title: "Password reset",
+        description: "User forgot their password",
+        priority: "Medium",
+        status: "Closed"
+    }
+];
+
 function App() {
     return (
         <div>
-            <TicketCard title= "Cannot connect to WIFI" description = "User cannot connect to the office WIFI" priority="High" status="Open"></TicketCard>
-            <TicketCard title= "Printer not working" description = "User cannot connect to office printer" priority= "Low" status="Open"></TicketCard>
-            <TicketCard title= "Password reset" description="User forgot password to their account" priority="Medium" status ="Closed"></TicketCard>
-        </div>
-              
-    )
+            {
+                tickets.map((ticket)=> {
+                    return  <TicketCard
+                    key={ticket.id}
+                    title={ticket.title}
+                    description={ticket.description}
+                    priority={ticket.priority}
+                    status={ticket.status} />
+                   
+                })
+            }
+        </div>            
+    );
 }
 
 export default App;
